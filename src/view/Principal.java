d0b8888efe33cb6bce518399acc89f5ef52527d8
Everton Santos <everton_santos_01@hotmail.com>
2021-11-11 21:01:18 -0300

    Primeiro commmit

---
+++ src/view/Principal.java
@@ -0,0 +1,29 @@
+package view;
+
+import controller.ListaController;
+import felix.utils.listaencadeada.integer.ListaEncadeadaSimples;
+import felix.utils.listaencadeada.integer.No;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		ListaController controller = new ListaController();
+		
+		ListaEncadeadaSimples lista1 = new ListaEncadeadaSimples();
+		ListaEncadeadaSimples lista2 = new ListaEncadeadaSimples();
+		
+		for (int i = 0; i < 5; i++) {
+			int num1 = (int) ((Math.random() * 200) + 1);
+			lista1.adicionaElemento(new No(num1));
+			
+			int num2 = (int) ((Math.random() * 200) + 1);
+			lista2.adicionaElemento(new No(num2));
+		}
+
+		System.out.println(lista2.toString());
+		
+		
+		controller.ascSort(lista2);
+	}
+
+}
